425de25ab465840668fca1cd57d321ad80629b09
chy0503 <202015006@kyonggi.ac.kr>
2023-09-18 22:55:20 +0900

    프로젝트 생성

---
+++ src/main/java/com/monsters/hanbang/HanbangApplication.java
@@ -0,0 +1,13 @@
+package com.monsters.hanbang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanbangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanbangApplication.class, args);
+	}
+
+}
